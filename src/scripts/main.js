e9ae59d4dce023e254b08ae18d644f52d8a1a067
A-Slastikhina <ao.slastikhina@gmail.com>
2022-04-24 19:00:12 +0300

    Доделала модальное окно - меню при помощи JS. По мелочи отрелактировала стили.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,36 @@
-var navMain = document.querySelector('.navMain');
+var navMain = document.querySelector('.nav__list');
 var burger = document.querySelector('.burger');
-var modalClose = document.querySelector ('.modalClose')
+var test = document.querySelector('.nav__item--test')
+
 burger.addEventListener('click', function(){
-    navMain.classList.remove('navMain--open')
-    burger.classList.remove('burger');
-    burger.classList.add('modalClose');
+   if (navMain.classList.contains ('nav--closed')) {
+    navMain.classList.remove('nav--closed');
+    navMain.classList.add ('nav--opened');   
+} 
+    else {
+    navMain.classList.remove ('nav--opened');
+    navMain.classList.add ('nav--closed');
+}
+});
+
+burger.addEventListener ('click', function(){
+    if (burger.classList.contains ('burger--open')){
+        burger.classList.remove('burger--open');
+        burger.classList.add('burger--close');  
+    }
+    else {
+        burger.classList.remove ('burger--close');
+        burger.classList.add('burger--open') ;
+
+}
+});
+
+burger.addEventListener ('click', function (){
+    if (burger.classList.contains('burger--close')){
+        test.classList.add('not-seen');
+    }
+    else {
+        test.classList.remove('not-seen');
+    }
 })
 
